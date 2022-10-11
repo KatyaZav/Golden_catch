eb1ed1932cdaacc801081f0703fbadffa81c888a
KatyaZav <party2015ru@mail.ru>
2022-10-11 16:32:25 +0500

    trying add new effect

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rb;
     [SerializeField] float speed;
+
+    [SerializeField] GameObject changeMoveEffect;
+    moveDirection _moveDirection;
+
 
     void Start()
     {
@@ -20,10 +25,30 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         float x = Input.GetAxis("Horizontal");
-        var movement = new Vector2(x, 0);
 
-        //Debug.Log(movement);
+        var movement = new Vector2(x, 0);
         _rb.AddForce(movement * speed);
-        //transform.Translate(movement * speed * Time.fixedDeltaTime);
+
+        CheckChangeDirection(x);
+               
     }
+
+    private void CheckChangeDirection(float x)
+    {
+        if (Math.Round(x) != (int)_moveDirection)
+        {
+            _moveDirection = (moveDirection)Math.Round(x);
+
+            Debug.Log("changed");
+            var ef = Instantiate(changeMoveEffect);
+            ef.transform.position = transform.position;
+        }
+    }
+
+    enum moveDirection
+    {
+        right = 1,
+        left = -1
+    };
 }
+
